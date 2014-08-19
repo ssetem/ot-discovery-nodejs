0e59deb1e8828998a5d40b68e28c4d619adf222a
Andy Royle <ajroyle@gmail.com>
2014-08-19 22:19:08 +0100

    add gruntfile with jshint task

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        jshint: {
+            all: [ '*.js' ]
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', ['jshint']);
+};
