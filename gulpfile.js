aabee244733e68901ac5f5e4a7dadadb107f513f
Tor Helmer <torhelmer@gmail.com>
2015-08-25 18:37:51 -0700

    changed to use ot-release-tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,136 +1,19 @@
-/*jslint node: true */
 'use strict';
-
-//mocha required options*****************
-require('should');
-//***************************************
 
 // Include gulp
 var gulp = require('gulp');
-var istanbul = require('gulp-istanbul');
-// We'll use mocha here, but any test framework will work
-var mocha = require('gulp-mocha');
-var isparta = require('isparta');
-var coverageEnforcer = require('gulp-istanbul-enforcer');
-var minimist = require('minimist');
-var _ = require('lodash');
-var coffeelint = require('gulp-coffeelint');
-var jshint = require('gulp-jshint');
-var logwarn = require('gulp-logwarn');
 
-var defaultPaths = {
-  scripts: ['src/**/*.coffee', 'src/**/*.js'],
-  tests: ['test/**/*.coffee', 'test/**/*.js'],
-  noCoverageTests: [],
-  coverage: 'coverage/',
-  zipCoverage: 'coverage/*'
+var scripts = ['src/**/*.coffee', 'src/**/*.js'];
+var tests = ['test/**/*.coffee', 'test/**/*.js'];
+var coverage = 'coverage/';
+var mochaOpts = {
+  require: ['./test/globals.js']
 };
-
-var logStr = ['console' + '.log'];
-
-var defaultCoverageThresholds = {
-  statements: 90,
-  branches: 50,
-  functions: 90,
-  lines: 80
-};
-
-function createFilteredPaths(normalPaths, toFilterPaths) {
-  return normalPaths.concat(toFilterPaths.map(function(item) {
-    return '!' + item;
-  }));
-}
-
-function filterPaths(filetype, pathObject) {
-  var filterFunc = function(path) {
-    return path.indexOf(filetype) > 0;
+var coverageThresholds = {
+    statements: 99.9,
+    branches: 98.5,
+    functions: 99.9,
+    lines: 99.9
   };
-  return _.filter(pathObject.scripts, filterFunc).concat(
-    _.filter(pathObject.tests, filterFunc));
-}
 
-function test(path, opts) {
-  opts = opts || {};
-
-  var mochaOpts = minimist(process.argv.slice(2), {
-    default: _.merge({
-      timeout: 7000,
-      require: ['./test/globals.js']
-    }, opts)
-  });
-
-  return gulp.src(path, {
-      read: false
-    })
-    .pipe(mocha(mochaOpts));
-}
-
-var coverageTest = function(cb, paths, mochaOpts) {
-  istanbul = require('gulp-coffee-istanbul');
-
-  var testsPaths = createFilteredPaths(paths.tests, paths.noCoverageTests);
-
-  gulp.src(paths.scripts)
-    .pipe(istanbul({
-      instrumenter: isparta.Instrumenter,
-      includeUntested: true
-    })) // Force `require` to return covered files
-    .pipe(istanbul.hookRequire()) // Force `require` to return covered files
-    .on('finish', function() {
-      test(testsPaths, mochaOpts)
-        .pipe(
-          istanbul.writeReports({
-            dir: paths.coverage,
-            reportOpts: {
-              dir: paths.coverage
-            },
-            reporters: ['text', 'text-summary', 'json', 'html', 'teamcity']
-          })
-        )
-        .pipe(
-          coverageEnforcer({
-            thresholds: defaultCoverageThresholds,
-            coverageDirectory: paths.coverage,
-            rootDirectory: ''
-          })
-        )
-        .on('finish', cb)
-        .on('error', cb);
-    });
-};
-
-gulp.task('test', function() {
-  require('coffee-script/register');
-  return test(defaultPaths.tests);
-});
-
-gulp.task('test-all-coverage', function(cb) {
-  coverageTest(cb, defaultPaths);
-});
-
-gulp.task('coffeelint', function() {
-  var src = filterPaths('.coffee', defaultPaths);
-  gulp.src(src)
-    .pipe(logwarn(logStr))
-    .pipe(coffeelint({
-      'max_line_length': {
-        'level': 'ignore'
-      }
-    }))
-    .pipe(coffeelint.reporter())
-    .pipe(coffeelint.reporter('fail'));
-});
-
-gulp.task('jslint', function() {
-  var src = filterPaths('.js', defaultPaths);
-  src.push('gulpfile.js');
-  return gulp.src(src)
-    .pipe(logwarn(logStr))
-    .pipe(jshint())
-    .pipe(jshint.reporter())
-    .pipe(jshint.reporter('fail'));
-});
-
-gulp.task('lint', ['jslint', 'coffeelint']);
-
-gulp.task('default', ['lint', 'test-all-coverage']);
+require('ot-gulp-release-tasks')(gulp, scripts, tests, mochaOpts, coverageThresholds);
